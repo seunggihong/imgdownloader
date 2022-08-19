d9b0315e13e0bb5e3ddfb292d4c5d19f38e8abef
onggi <hsk1533@naver.com>
2022-08-19 18:32:38 +0900

    image downloader

---
+++ script.js
@@ -0,0 +1,35 @@
+const fileInput = document.querySelector("input"),
+  downloadBtn = document.querySelector("button");
+
+downloadBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  downloadBtn.innerText = "Downloading file...";
+  fetchFile(fileInput.value);
+});
+
+function fetchFile(url) {
+  fetch(url)
+    .then((res) => res.blob())
+    .then((file) => {
+      let tempUrl = URL.createObjectURL(file);
+      const aTag = document.createElement("a");
+      aTag.href = tempUrl;
+      aTag.download = url.replace(/^.*[\\\/]/, "");
+      document.body.appendChild(aTag);
+      aTag.click();
+      downloadBtn.innerText = "Download File";
+      URL.revokeObjectURL(tempUrl);
+      aTag.remove();
+    })
+    .catch(() => {
+      alert("Failed to download file!");
+      downloadBtn.innerText = "Download File";
+    });
+}
+// youtube url parser
+function youtube_parser(url) {
+  var regExp =
+    /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+  var match = url.match(regExp);
+  return match && match[7].length == 11 ? match[7] : false;
+}
